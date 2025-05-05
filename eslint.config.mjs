3cfacf5d4c0fd959d1ae1ef3c8744f92049f078a
Pete Balsavias <pete@grunge.ai>
2025-05-05 16:48:16 -0500

    local updates for fix to multi-module issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ export default tseslint.config(
         rules: {
             "@typescript-eslint/no-unused-vars": ["error",
                 { "argsIgnorePattern": "^_" }
-            ]
+            ],
+			"@typescript-eslint/no-explicit-any": "off"
         }
     }
 );
